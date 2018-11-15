3bb9e54648b31f529f9a596cd05ad56f3acb5436
Peiyao Li <462046122@qq.com>
2018-11-15 14:47:06 +0800

    maxheap

--- DataStructure/DataStructure.hpp
+++ DataStructure/DataStructure.hpp
@@ -20,6 +20,8 @@ namespace dataStructure {
 	class arrayQueue;
 	template <class T>
 	class arrayStack;
+	template <class T>
+	class maxHeap;
 }
 
 using namespace dataStructure;
@@ -108,4 +110,64 @@ size_t binaryTree<T>::recursiveGetDepth(binaryTreeNode<T>* t) {
 template <class T>
 size_t binaryTree<T>::depth() {
 	return recursiveGetDepth(root);
+}
+
+template <class T>
+class dataStructure::maxHeap {
+private:
+	T* arr;
+	size_t size;
+	size_t capacity;
+	void shiftUp(size_t index);
+//	void shiftDowm(size_t index);
+public:
+	maxHeap();
+//	maxHeap(T* arr, size_t size);
+	maxHeap(size_t capacity);
+	bool insert(T& element);
+	void printWayToRoot(size_t index);
+};
+
+template <class T>
+maxHeap<T>::maxHeap() {
+	arr = NULL;
+	size = capacity = 0;
+}
+
+template <class T>
+maxHeap<T>::maxHeap(size_t cap) {
+	capacity = cap;
+	arr = new T[capacity + 1];
+	size = 0;
+}
+
+template <class T>
+void maxHeap<T>::shiftUp(size_t index) {
+	if (index == 1) return;
+	else if (arr[index] > arr[index / 2]) {
+		T temp = arr[index / 2];
+		arr[index / 2] = arr[index];
+		arr[index] = temp;
+		shiftUp(index / 2);
+	}
+	else return;
+}
+
+template <class T>
+bool maxHeap<T>::insert(T& element) {
+	size++;
+	arr[size] = element;
+	shiftUp(size);
+	return true;
+}
+
+template <class T>
+void maxHeap<T>::printWayToRoot(size_t index) {
+	while (index >= 1)
+	{
+		std::cout << arr[index];
+		if (index != 1) std::cout << ' ';
+		index /= 2;
+	}
+	std::cout << std::endl;
 }
--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -7,11 +7,27 @@ using std::string;
 using namespace dataStructure;
 
 int main() {
-	binaryTree<char> tree;
-	string a, b;
-	cin >> a >> b;
-	tree.makeTree(b, a);
-	cout << tree.depth();
+	int n, m;
+	cin >> n >> m;
+	maxHeap<int> heap(n);
+	int* arr = new int[m];
+	int k = 0;
+	while (true) {
+		if (k > n - 1) break;
+		int t;
+		cin >> t;
+		heap.insert(t);
+		k++;
+	}
+	k = 0;
+	while (true) {
+		if (k > m - 1) break;
+		cin >> arr[k];
+		k++;
+	}
+	for (int i = 0; i < m; i++) {
+		heap.printWayToRoot(arr[i]);
+	}
 	system("pause");
 	return 0;
 }
