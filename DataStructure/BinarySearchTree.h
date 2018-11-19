e77ba5a7c63d5cf66b6a883806a33ae60456778d
Peiyao Li <462046122@qq.com>
2018-11-20 01:25:19 +0800

    bsTree ver 0.1

--- DataStructure/BinarySearchTree.h
+++ DataStructure/BinarySearchTree.h
@@ -5,5 +5,28 @@
 #include <string>
 using std::string;
 #endif // !STRING_H
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+#include "binaryTree.h"
+using dataStructure::binaryTree;
+using dataStructure::binaryTreeNode;
+#endif // !BINARYTREE_H
 
 
+namespace dataStructure {
+	template <class T>
+	class binarySearchTree;
+}
+
+using dataStructure::binarySearchTree;
+
+template <class T>
+class dataStructure::binarySearchTree : public binaryTree<T> {
+public:
+	binarySearchTree() :binaryTree<T>::binaryTree() { };
+//	void makeTree(string& inOrder, string& preOrder);
+//	binaryTreeNode<T>* search(T& theKey);
+//	void insert(T& theKey);
+//	void deleteNode(T& theKey);
+//	void ascent();
+};
--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -7,27 +7,11 @@ using std::string;
 using namespace dataStructure;
 
 int main() {
-	int n, m;
-	cin >> n >> m;
-	maxHeap<int> heap(n);
-	int* arr = new int[m];
-	int k = 0;
-	while (true) {
-		if (k > n - 1) break;
-		int t;
-		cin >> t;
-		heap.insert(t);
-		k++;
-	}
-	k = 0;
-	while (true) {
-		if (k > m - 1) break;
-		cin >> arr[k];
-		k++;
-	}
-	for (int i = 0; i < m; i++) {
-		heap.printWayToRoot(arr[i]);
-	}
+	binarySearchTree<char> bst;
+	string a, b;
+	cin >> a >> b;
+	bst.makeTree(b, a);
+	cout << bst.depth();
 	system("pause");
 	return 0;
 }
