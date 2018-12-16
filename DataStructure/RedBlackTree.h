56cc08831a0c59951877803bda50cb2494ae3dcd
Peiyao Li <462046122@qq.com>
2018-12-16 22:10:34 +0800

    RBTree 0.1

--- DataStructure/RedBlackTree.h
+++ DataStructure/RedBlackTree.h
@@ -34,6 +34,10 @@ namespace dataStructure {
 		RBTreeNode<T>* root;
 		int nodeNum;
 
+		RBTreeNode<T>* leftRotation(RBTreeNode<T>* node);
+		RBTreeNode<T>* rightRotation(RBTreeNode<T>* node);
+		RBTreeNode<T>* leftRightRotation(RBTreeNode<T>* node);
+		RBTreeNode<T>* rightLeftRotation(RBTreeNode<T>* node);
 	public:
 		RBTree() { root = NULL; nodeNum = 0; };
 
@@ -45,6 +49,23 @@ namespace dataStructure {
 	};
 
 	template <class T>
+	RBTreeNode<T>* RBTree<T>::leftRotation(RBTreeNode<T>* node) {
+		RBTreeNode<T>* newTree = node->getRight();
+		
+		
+
+		return newTree;
+	}
+
+	template <class T>
+	RBTreeNode<T>* RBTree<T>::rightRotation(RBTreeNode<T>* node) {
+		RBTreeNode<T>* newTree = node->getLeft();
+
+
+		return newTree;
+	}
+
+	template <class T>
 	RBTreeNode<T>* RBTree<T>::find(const T& theKey) {
 		if (root == NULL) return NULL;
 
@@ -52,8 +73,14 @@ namespace dataStructure {
 		while (t) {
 			if (t->getData() == theKey) return t;
 
-			if (theKey < t->getData()) t = t->getLeft();
-			else if (theKey > t->getData()) t = t->getRight();
+			if (theKey < t->getData()) {
+				if (t->getLeft()) t = t->getLeft();
+				else return t;
+			}
+			else if (theKey > t->getData()) {
+				if(t->getRight()) t = t->getRight();
+				else return t;
+			}
 		}
 
 		return t;
@@ -76,7 +103,7 @@ namespace dataStructure {
 
 	template <class T>
 	void RBTree<T>::insert(const T& theKey) {
-		BTreeNode<T>* newNode = new BTreeNode<T>(theKey);
+		RBTreeNode<T>* newNode = new RBTreeNode<T>(theKey);
 
 		if (root = NULL) {
 			root = newNode;
@@ -84,6 +111,20 @@ namespace dataStructure {
 			return;
 		}
 
-		BTreeNode<T>* target = 
+		newNode->setColor(red);
+		RBTreeNode<T>* target = find(theKey);
+
+		if (target->getData() == theKey) return;
+
+		RBTreeNode<T>* ancestor = target->getAncestor();
+
+		if (target->getColor() == black) {
+			if (theKey < target->getColor()) target->setLeft(newNode);
+			else target->setRight(newNode);
+		}
+		else {
+			//If target is red, target must have a black ancestor
+
+		}
 	}
 }
